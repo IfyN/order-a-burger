59ce70e8fba02dbcdce8de2f9a0d12a43edd16af
IfyN <ifynwosu0@gmail.com>
2020-08-01 22:33:46 +0100

    added new components using state hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 import { render } from '@testing-library/react';
 
-class App extends Component {
+const app = props => {
+ const stateArr = useState({
+  persons: [
+    {name: 'Ifeoma', age: '25'},
+    {name: 'Chichi', age: '17'},
+    {name: 'Somto', age: '20'}
+  ],
+  otherState: 'some value'
+ })
+
+render() {
+  return (
+    <div className="App">
+      <h1>React App Test</h1>
+      <h1> Its working!</h1>
+      <button onClick={this.switchNameHandler}> Switch name</button>
+      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
+      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
+  </div>
+  );
+}
+}
+
+
+
+export default app;
+
+
+
 state = {
   persons: [
     {name: 'Ifeoma', age: '25'},
@@ -24,21 +53,3 @@ switchNameHandler = () =>{
     }
   )
 }
-
-render() {
-  return (
-    <div className="App">
-      <h1>React App Test</h1>
-      <h1> Its working!</h1>
-      <button onClick={this.switchNameHandler}> Switch name</button>
-      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
-      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
-  </div>
-  );
-}
-}
-
-
-
-export default App;
