2ba966a77f11210d9dd173c10dd9ea76ede66c1f
IfyN <ifynwosu0@gmail.com>
2020-08-01 19:27:57 +0100

    added state

--- src/App.js
+++ src/App.js
@@ -8,24 +8,29 @@ state = {
   persons: [
     {name: 'Ifeoma', age: '25'},
     {name: 'Chichi', age: '17'},
-    {name: 'Somto', age: '20'},
+    {name: 'Somto', age: '20'}
   ]
 }
+
+switchNameHandler = () =>{
+  //console.log('Was clicked');
+  this.state.persons[0].name = Maximillia
 }
 
-render(){
-  return(
+render() {
+  return (
     <div className="App">
       <h1>React App Test</h1>
       <h1> Its working!</h1>
-      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}> Habelo
-      </Person>
-      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}> Habelo
-      </Person>
-      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}> Habelo
-      </Person>
-    </div>
-  )
+      <button onClick={this.switchNameHandler}> Switch name</button>
+      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
+      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
+      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
+  </div>
+  );
 }
+}
+
+
 
 export default App;
