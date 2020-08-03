3ee2ddb8d4d8550f3a5aa2d01746a55ffebe1b02
IfyN <ifynwosu0@gmail.com>
2020-08-03 21:39:11 +0100

    made use of functional component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 import { render } from '@testing-library/react';
 
 const app = props => {
- const stateArr = useState({
+ const [personState, setPersonsState] = useState({
   persons: [
     {name: 'Ifeoma', age: '25'},
     {name: 'Chichi', age: '17'},
     {name: 'Somto', age: '20'}
   ],
   otherState: 'some value'
- })
+ });
 
 render() {
   return (
@@ -19,9 +19,9 @@ render() {
       <h1>React App Test</h1>
       <h1> Its working!</h1>
       <button onClick={this.switchNameHandler}> Switch name</button>
-      <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-      <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}/>
-      <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
+      <Person name = {personState.persons[0].name} age = {personState.persons[0].age}/>
+      <Person name = {personState.persons[1].name} age = {personState.persons[1].age}/>
+      <Person name = {personState.persons[2].name} age = {personState.persons[2].age}/>
   </div>
   );
 }
@@ -32,14 +32,6 @@ render() {
 export default app;
 
 
-
-state = {
-  persons: [
-    {name: 'Ifeoma', age: '25'},
-    {name: 'Chichi', age: '17'},
-    {name: 'Somto', age: '20'}
-  ]
-}
 
 switchNameHandler = () =>{
   //console.log('Was clicked');
